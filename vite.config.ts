7487ac3c5d07d5238f4678a077889fdc125f7d50
AzaDork <antoni.caulier@gmail.com>
2025-04-30 20:40:39 +0200

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GalerieMNC_LandingPage/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
