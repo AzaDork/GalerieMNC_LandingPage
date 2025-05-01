11df2504cdc4be88cbe3e371c5f7e9c08cd6e32f
AzaDork <antoni.caulier@gmail.com>
2025-05-01 13:37:46 +0200

    déploiement de la page une fois que le header s'affiche

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import React, { useEffect } from 'react';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Introduction from './components/Introduction';
+import Nouveaute from './components/Nouveaute';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+import { setupAnimations } from './utils/animations';
 
 function App() {
+  useEffect(() => {
+    setupAnimations();
+    
+    // Update page title
+    document.title = 'Galerie MNC | Art Contemporain & Encadrement';
+  }, []);
+
   return (
     <div className="font-sans text-gray-900">
       <Header />
+      <Hero />
+      <Introduction />
+      <Nouveaute />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
