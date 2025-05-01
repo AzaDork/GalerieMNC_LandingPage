af8b2f8a0f9720656e32562a0effb3b03fb2a3d5
AzaDork <antoni.caulier@gmail.com>
2025-05-01 18:08:40 +0200

    final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header: React.FC = () => {
     >
       <div className="container mx-auto px-4 md:px-8">
         <div className="flex items-center justify-between">
-          <a href="/" className="flex items-center h-26">
+          <a href="/" className="flex items-center h-24">
             <img 
               src={logo}
               alt="Galerie MNC" 
