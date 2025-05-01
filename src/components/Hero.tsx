2cd8b57a0c64ba28627f8ef65b5f3f59e67b0e0f
AzaDork <antoni.caulier@gmail.com>
2025-05-01 13:24:18 +0200

    update jpg to jpeg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <div className="absolute inset-0">
         <div className="absolute inset-0 bg-black bg-opacity-30 z-10" />
         <img
-          src="/GalerieMNC_vitrine.jpg"
+          src="/GalerieMNC_vitrine.jpeg"
           alt="Galerie MNC Storefront"
           className="w-full h-full object-cover"
         />
