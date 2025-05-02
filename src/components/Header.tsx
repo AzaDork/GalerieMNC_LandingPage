e1e0f8680549521f9b1a1036e3a09b6bf602d01a
AzaDork <antoni.caulier@gmail.com>
2025-05-02 19:33:24 +0200

    final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header: React.FC = () => {
 
   return (
     <header 
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+      className={`fixed top-0 left-0 right-0 z-[9999] transition-all duration-300 ${
         scrolled ? 'shadow-md py-3' : 'py-6'
       }`}
       style={{ backgroundColor: headerBg }}
